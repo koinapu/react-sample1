fd35376fbb0938b8fb6aa9841cfedc985382b93a
koinapu <koi.inapu@gmail.com>
2021-05-02 12:39:45 +0900

    Initial commit

---
+++ src/components/pages/Top.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
+import { useHistory } from "react-router-dom";
+//import { UserContext } from "../../providers/UserProvider";
+import { useSetRecoilState } from "recoil";
+import { userState } from "../../store/userState";
+
+export const Top = () => {
+  const history = useHistory();
+  //const { setUserInfo } = useContext(UserContext);
+  const setUserInfo = useSetRecoilState(userState);
+
+  const onClickAdmin = () => {
+    setUserInfo({ isAdmin: true });
+    history.push({ pathname: "users" });
+  };
+  const onClickGeneral = () => {
+    setUserInfo({ isAdmin: false });
+    history.push({ pathname: "users" });
+  };
+  return (
+    <SContainer>
+      <h2>TOPページです</h2>
+      <SecondaryButton onClick={onClickAdmin}>管理者ユーザ</SecondaryButton>
+      <br />
+      <br />
+      <SecondaryButton onClick={onClickGeneral}>一般ユーザ</SecondaryButton>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  text-align: center;
+`;
